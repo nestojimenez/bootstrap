d8b1e29dfc8753186c15d6bc1773ebd848187929
nestojimenez <aguiladescalza@gmail.com>
2022-12-06 16:36:52 -0800

    Load Project Form init

--- project_main.js
+++ project_main.js
@@ -1,1 +1,5 @@
-onmousemove = function(e){console.log(`mouse location = X: ${e.x}, Y: ${e.y}`)}
+/*
+let container = document.getElementById('step')
+onmousemove = function(e){container.innerHTML = '<h1 class="h3 fw-bold text-white">Step 2: Understand</h1> <p class="col-md-8 fs-4 text-white">' + e.x +'   '+ e.y + 'Automation team should understand the need by reviewing CTQs, Anual Volume, Product Design, Estimated Layout.</p>';
+};
+*/
